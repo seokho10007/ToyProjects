e4dcce221b229646f3c34c2c56f48dfc8082e6eb
seokho10007 <sukho1007@naver.com>
2021-10-05 22:48:19 +0900

    Header Section 추가

---
+++ emotion_recoil_todo/src/components/UI/organisms/Header/HeaderContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderSection from '@organisms/HeaderSection';
+
+const HeaderContainer: React.FC = () => {
+	return (
+		<>
+			<HeaderSection />
+		</>
+	);
+};
+
+export default HeaderContainer;
