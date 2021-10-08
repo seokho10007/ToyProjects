9997019be29cbd32069881af75de56adfcc5df49
seokho10007 <sukho1007@naver.com>
2021-10-08 22:51:15 +0900

    Style. Emotion Theme 설정 적용

--- emotion_recoil_todo/src/theme/globalStyle.tsx
+++ emotion_recoil_todo/src/theme/globalStyle.tsx
@@ -2,36 +2,38 @@ import React from 'react';
 import { Global, css } from '@emotion/react';
 import reset from 'emotion-reset';
 import { ThemeType } from './index';
+import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
 
 interface GlobalStyleProps {
 	theme: ThemeType;
 }
 
-const GlobalStyle = ({ theme }: GlobalStyleProps) => (
+const GlobalStyle = ({ theme }: GlobalStyleProps): EmotionJSX.Element => (
 	<Global
 		styles={css`
 			${reset}
 
-			html, body, #__next {
+			html, body, #root {
 				height: 100%;
+				background-color: ${theme.BACKGROUND_COLOR.SECOND_COLOR};
 			}
 			html,
 			body {
 				overflow-x: hidden;
-				width: 100%;
-				height: 100%;
-				color: ${theme.PRIMARY_COLOR};
 			}
 			body {
 				overflow-y: overlay;
 				margin: 0;
-				font-size: 14px;
+				font-size: 16px;
 				line-height: 1.5715;
 				user-select: none;
+				width: 100%;
+				height: 100%;
 			}
 			a {
 				text-decoration: none;
 				outline: none;
+				color: ${theme.FONT_COLOR.PRIMARY_COLOR};
 			}
 			& * {
 				font-family: 'Noto Sans KR', sans-serif !important;
