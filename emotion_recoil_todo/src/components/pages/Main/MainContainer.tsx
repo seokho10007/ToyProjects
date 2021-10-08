baa864f6cab2b2fe3444202d76296fd792b8e8f0
seokho10007 <sukho1007@naver.com>
2021-10-08 22:46:44 +0900

    Style. Main Compoenent 스타일 적용

--- emotion_recoil_todo/src/components/pages/Main/MainContainer.tsx
+++ emotion_recoil_todo/src/components/pages/Main/MainContainer.tsx
@@ -1,9 +1,37 @@
+import styled from '@emotion/styled';
 import React from 'react';
+import RowFrame from '@frames/RowFrame';
+import TodoItem from '@molecules/TodoItem/TodoItem';
+
+const StyledMainContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding: 40px 0;
+	background-color: ${({ theme }) => theme.BACKGROUND_COLOR.SECOND_COLOR};
+
+	& > div {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-around;
+
+		@media (max-width: ${({ theme }) => theme.BP.PC}) {
+			flex-direction: column;
+		}
+	}
+`;
 
 const MainContainer: React.FC = () => {
 	return (
 		<>
-			<div>Main</div>
+			<StyledMainContainer>
+				<RowFrame>
+					<TodoItem title="오늘 할 일" />
+					<TodoItem title="완료" />
+				</RowFrame>
+			</StyledMainContainer>
 		</>
 	);
 };
