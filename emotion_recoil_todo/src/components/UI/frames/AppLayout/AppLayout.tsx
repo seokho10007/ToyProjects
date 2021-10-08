d6ccfb2f36d6062a0d94ddb5a12b26cd10aed674
seokho10007 <sukho1007@naver.com>
2021-10-08 22:47:28 +0900

    Footer 추가

--- emotion_recoil_todo/src/components/UI/frames/AppLayout/AppLayout.tsx
+++ emotion_recoil_todo/src/components/UI/frames/AppLayout/AppLayout.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import styled from '@emotion/styled';
-import Header from '../../organisms/Header';
-// import Header from '@organisms/Header';
+import Header from '@organisms/Header';
+import Footer from '@organisms/Footer';
 
 const StyledAppLayout = styled.div`
-	min-height: calc(100% - 140px);
+	height: calc(100% - 100px);
 	position: relative;
-	padding-bottom: 140px;
-	background-color: ${({ theme }) => theme.BACKGROUND_COLOR};
+	padding-bottom: 100px;
 `;
 
 const AppLayout: React.FC = ({ children }) => (
@@ -16,6 +15,7 @@ const AppLayout: React.FC = ({ children }) => (
 		<StyledAppLayout>
 			<Header />
 			{children}
+			<Footer />
 		</StyledAppLayout>
 	</>
 );
--- emotion_recoil_todo/src/components/UI/organisms/Footer/FooterContainer.tsx
+++ emotion_recoil_todo/src/components/UI/organisms/Footer/FooterContainer.tsx
@@ -1,9 +1,39 @@
+import styled from '@emotion/styled';
 import React from 'react';
+import RowFrame from '../../frames/RowFrame';
+
+const StyledFooterContainer = styled.div`
+	width: 100%;
+	height: 80px;
+	position: absolute;
+	background-color: ${({ theme }) => theme.BACKGROUND_COLOR.SECOND_COLOR};
+	& > div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+`;
+
+const FooterInner = styled.div`
+	width: 100%;
+	padding: 0 10px;
+	border-top: 1px solid #cccccc;
+	height: 100%;
+	flex-direction: row;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
 
 const FooterContainer: React.FC = () => {
 	return (
 		<>
-			<div>Footer</div>
+			<StyledFooterContainer>
+				<RowFrame>
+					<FooterInner>Footer</FooterInner>
+				</RowFrame>
+			</StyledFooterContainer>
 		</>
 	);
 };
