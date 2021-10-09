b1e32b7946e5857b5fa5ce33a8b0326d87a80b85
seokho10007 <sukho1007@naver.com>
2021-10-09 22:54:06 +0900

    Style. Molecules Component 추가

--- emotion_recoil_todo/src/components/UI/molecules/Logo/Logo.tsx
+++ emotion_recoil_todo/src/components/UI/molecules/Logo/Logo.tsx
@@ -1,18 +1,26 @@
 import React, { ReactElement } from 'react';
 import styled from '@emotion/styled';
+import LinkBox from '@atoms/LinkBox';
 
 const StyledLogo = styled.div`
 	font-size: ${({ theme }) => theme.FONT_SIZE.LOG_SIZE};
 	font-weight: bold;
-
+	transition: all 0.2s linear;
 	cursor: pointer;
+
+	&:hover {
+		transform: scale(1.1);
+	}
 `;
 
 interface Props {
 	logoName: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const Logo = ({ logoName }: Props): ReactElement => <StyledLogo>{logoName}</StyledLogo>;
+const Logo = ({ logoName }: Props): ReactElement => (
+	<StyledLogo>
+		<LinkBox name={logoName} src="/" />
+	</StyledLogo>
+);
 
 export default Logo;
