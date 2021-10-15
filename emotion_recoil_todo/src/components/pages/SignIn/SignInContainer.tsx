7485b02950ca1c007219b84aef6c72e1d4ff6801
seokho10007 <sukho1007@naver.com>
2021-10-15 23:32:22 +0900

    SignIn Form 작성

--- emotion_recoil_todo/src/components/pages/SignIn/SignInContainer.tsx
+++ emotion_recoil_todo/src/components/pages/SignIn/SignInContainer.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import RowFrame from '@frames/RowFrame';
+import SignInForm from '@molecules/SignInForm';
+
+const StyledSignInForm = styled.div`
+	width: 100%;
+`;
 
 const SignInContainer: React.FC = () => {
 	return (
 		<>
-			<div>SignIn</div>
+			<StyledSignInForm>
+				<RowFrame>
+					<SignInForm />
+				</RowFrame>
+			</StyledSignInForm>
 		</>
 	);
 };
