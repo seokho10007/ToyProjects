f4a70e890647636814b29e7dd4fd1d72ab5ac08c
seokho10007 <sukho1007@naver.com>
2021-10-17 23:33:26 +0900

    Feat. axios api 요청 실행

--- emotion_recoil_todo/src/api/lists.ts
+++ emotion_recoil_todo/src/api/lists.ts
@@ -9,3 +9,11 @@ export const addList = (title: string, type: string) => {
 
 	return content;
 };
+
+export const deleteItem = (id: string) => {
+	// 비동기 통신, id값에 따라 아이템 삭제
+};
+
+export const changeItemCompletion = (id: string) => {
+	// 비동기 통신, id값에 아이템 상태 변경
+};
