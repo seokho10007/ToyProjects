85d3a902836df910f09774771fe9429882a52bfd
seokho10007 <sukho1007@naver.com>
2021-10-20 01:00:10 +0900

    Feat. 로그인시 Nav State 변경

--- emotion_recoil_todo/src/components/UI/molecules/Nav/Nav.tsx
+++ emotion_recoil_todo/src/components/UI/molecules/Nav/Nav.tsx
@@ -1,6 +1,11 @@
+import React, { useCallback } from 'react';
+import axios from 'axios';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import styled from '@emotion/styled';
-import React from 'react';
-import LinkBox from '../../atoms/LinkBox';
+import LinkBox from '@atoms/LinkBox';
+import Button from '@atoms/Button/DefaultButton';
+import { userState } from '@states/Atoms';
+import { signoutStatas } from '@states/Selectors';
 
 const StyledNav = styled.div`
 	display: flex;
@@ -11,11 +16,28 @@ const StyledNav = styled.div`
 `;
 
 const Nav: React.FC = () => {
+	const userInfo = useRecoilValue(userState);
+	const onResetUserState = useSetRecoilState(signoutStatas);
+
+	const onSignout = useCallback(async () => {
+		await axios.post('auth/signout');
+		onResetUserState(null);
+	}, [onResetUserState]);
+
 	return (
 		<>
 			<StyledNav>
-				<LinkBox src="/signup" name="회원가입" />
-				<LinkBox src="/signin" name="로그인" />
+				{userInfo ? (
+					<>
+						<div>{userInfo.username}</div>
+						<Button onClick={onSignout} name="로그아웃" />
+					</>
+				) : (
+					<>
+						<LinkBox src="/signup" name="회원가입" />
+						<LinkBox src="/signin" name="로그인" />
+					</>
+				)}
 			</StyledNav>
 		</>
 	);
