d5f32ab5f62a88a8e4a45b2cb875413c6af40542
seokho10007 <sukho1007@naver.com>
2021-12-13 22:55:42 +0900

    Feat. MongoDB 연결 및 Passport Jwt 설정

--- express-mongoDB/src/app.ts
+++ express-mongoDB/src/app.ts
@@ -1,26 +1,28 @@
 import express, { Application } from 'express';
 import mongoose from 'mongoose';
+import passport from 'passport';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
 
-import postsRouter from './routes/posts';
+import routes from '@src/routes';
 import { errorMiddleware } from './middlewares/errorMiddleware';
+import { PassportConfigunation } from './passport';
 
 dotenv.config();
 
 const uri = `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@simple-board-cluster.aat6r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 mongoose.connect(uri);
 
-mongoose.connection.on('connected', (err: Error) => {
+mongoose.connection.on('connected', () => {
 	console.log('MongoDB Connected');
 });
+
+PassportConfigunation();
 
 const app: Application = express();
 
 app.set('views', 'views');
-app.set('view engine', 'pug');
-
 // Body parsing Middleware
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,7 +30,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('public'));
 
-app.use('/posts', postsRouter);
+app.use(passport.initialize());
+
+app.get('/', (_, res) => {
+	res.send('express server');
+});
+
+app.use(routes);
 
 app.use(errorMiddleware);
 
