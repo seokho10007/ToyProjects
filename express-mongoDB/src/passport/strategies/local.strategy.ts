319b60fb5cf37b26e1d837a3ae619b423fbd69e2
seokho10007 <sukho1007@naver.com>
2021-12-14 23:18:22 +0900

    Chore. 사용하지 않는 모듈 제거

--- express-mongoDB/src/passport/strategies/local.strategy.ts
+++ express-mongoDB/src/passport/strategies/local.strategy.ts
@@ -1,6 +1,5 @@
 import { Strategy as LStrategy } from 'passport-local';
 import { authService } from '@src/services/auth.service';
-import * as bcrypt from 'bcrypt';
 
 export const LocalStrategy = new LStrategy(
 	{
@@ -11,11 +10,7 @@ export const LocalStrategy = new LStrategy(
 		try {
 			const user = await authService.validateUser(email, password);
 
-			console.log(user);
-
 			if (!user) return done(null, false, { message: '오류가 발생했습니다.' });
-
-			console.log('----');
 
 			return done(null, user);
 		} catch (error) {
