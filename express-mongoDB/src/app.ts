e54663055bcbd632651f2a6f52b0bd3f067eb5d3
seokho10007 <sukho1007@naver.com>
2021-12-14 23:14:13 +0900

    Feat. Express User 전역 타입 추가

--- express-mongoDB/src/app.ts
+++ express-mongoDB/src/app.ts
@@ -8,8 +8,11 @@ import dotenv from 'dotenv';
 import routes from '@src/routes';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 import { PassportConfigunation } from './passport';
+import { setAuth } from './middlewares/setAuth';
 
 dotenv.config();
+
+PassportConfigunation();
 
 const uri = `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@simple-board-cluster.aat6r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 mongoose.connect(uri);
@@ -18,23 +21,15 @@ mongoose.connection.on('connected', () => {
 	console.log('MongoDB Connected');
 });
 
-PassportConfigunation();
-
 const app: Application = express();
 
-app.set('views', 'views');
-// Body parsing Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static('public'));
 
 app.use(passport.initialize());
-
-app.get('/', (_, res) => {
-	res.send('express server');
-});
+app.use(setAuth);
 
 app.use(routes);
 
